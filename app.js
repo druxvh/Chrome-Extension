949a5d1e9165cf5eb7375ef1e8fba42f59f8f087
DM <druavhm@gmail.com>
2023-10-25 14:22:32 +0530

    Added SaveTab & Clear btns along with some JS code refactor

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 let saveSite = [];
 const inputText = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const saveTab = document.getElementById("tab-btn")
+const clearFunc = document.getElementById("clear-btn")
+
 const ulEl = document.getElementById("list-el");
-let savedInLS = JSON.parse(localStorage.getItem("saveSite"))
+const savedInLS = JSON.parse(localStorage.getItem("saveSite"));
 
 inputBtn.addEventListener("click", function () {
   //   console.log("Event List test");
   saveSite.push(inputText.value);
-  inputText.value = ""
-  localStorage.setItem("saveSite", JSON.stringify(saveSite))
-  renderSites()
+  inputText.value = "";
+  localStorage.setItem("saveSite", JSON.stringify(saveSite));
+  render(saveSite);
   // console.log(localStorage.getItem("saveSite"))
-
+  console.log("Save  it test")
 });
 
- if (savedInLS) {
-  saveSite = savedInLS
-  renderSites()
- } else {
-  alert("No Data Saved")
- }
+saveTab.addEventListener("click", function() {
+  console.log("Save Tab Test")
+})
+clearFunc.addEventListener("click", function() {
+  console.log("Clear Tab Test")
+})
 
-function renderSites() {
+
+
+if (savedInLS) {
+  //if (truthy)
+  saveSite = savedInLS;
+  render(saveSite);
+} else {
+  alert("No Data Saved");
+}
+
+function render(saveSite) {
   let listItems = " ";
 
   for (i = 0; i < saveSite.length; i++) {
@@ -31,13 +44,11 @@ function renderSites() {
       <a href= '${saveSite[i]}'> ${saveSite[i]} </a>
     </li>
     `;
-                //Developer Way of writing the same code but professionally
-            // let li = document.createElement("li")
-            // li.textContent = saveSite[i]
-            // ulEl.append(li)
+    //Developer Way of writing the same code but professionally
+    // let li = document.createElement("li")
+    // li.textContent = saveSite[i]
+    // ulEl.append(li)
   }
 
   ulEl.innerHTML = listItems;
 }
-
-
